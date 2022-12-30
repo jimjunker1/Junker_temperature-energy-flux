4c1aaaf1ad585ea610019aae523c3ccb13e7575f
Jim Junker <james.junker1@gmail.com>
2022-12-30 16:45:42 -0600

    trying to find a way to reduce packages and ensure compatibility

--- packages.R
+++ packages.R
@@ -2,23 +2,22 @@
 ###load packages and functions
   if(!require("pacman")) install.packages("pacman")
   library(pacman)
-  # package.list <- c("conflicted", "dotenv", "drake","data.table","gtools","rlist",
-  #                   "RCurl","plyr","tidyverse","furrr", "fnmate", "moments","fuzzySim",
-  #                   "dflow","rmarkdown","tictoc","chron","lubridate","httr","TTR", 
-  #                   "grid","gridExtra", "ggridges", "fluxweb", "rmarkdown", "MuMIn","zoib",
-  #                   "viridis", "broom","bbmle","ggthemes", "ggeffects", "ggpubr","betareg",
-  #                   "fluxweb","igraph","ggraph","magick","cowplot","rriskDistributions",
-  #                   "rstan", "brms", "tidybayes", "parallel", "hillR", "RInSp", "rsample",
-  #                   "emmeans")
-  package.list <- c('conflicted', 'dotenv', 'drake', 'abind', 'brms','cowplot',
-                    'dplyr', 'egg', 'furrr', 'ggplot2', 'ggpubr', 'grid', 'gridExtra',
-                    'loo', 'lubridate', 'plyr', 'purrr', 'RInSp', 'rlist',
-                    'rriskDistributions', 'stringr', 'tibble', 'tidyr', 'vegan',
-                    'viridis')
+  package.list <- c("conflicted", "dotenv", "drake","data.table","gtools","rlist",
+                    "RCurl","plyr","tidyverse","furrr", "fnmate", "moments","fuzzySim",
+                    "dflow","tictoc","chron","lubridate","httr","TTR",
+                    "grid","gridExtra", "ggridges", "MuMIn",
+                    "viridis", "broom","bbmle","ggthemes", "ggeffects", "ggpubr","betareg",
+                    "igraph","ggraph","magick","cowplot","rriskDistributions",
+                    "rstan", "brms", "tidybayes", "parallel", "hillR", "RInSp", "rsample",
+                    "emmeans")
+  # package.list <- c('conflicted', 'dotenv', 'drake', 'abind', 'brms','cowplot',
+  #                   'dplyr', 'egg', 'fnmate', 'furrr', 'ggplot2', 'ggpubr', 'grid', 'gridExtra',
+  #                   'loo', 'lubridate', 'plyr', 'purrr', 'RInSp', 'rlist',
+  #                   'rriskDistributions', 'rstan', 'stringr', 'tibble', 'tidyr', 'vegan',
+  #                   'viridis')
   p_load(char = package.list, install = TRUE, character.only = TRUE)
-  pacman::p_load_gh("jimjunker1/junkR")
-  # library(junkR)
-  # remotes::install_github("jimjunker1/junkR")
+  remotes::install_github("jimjunker1/junkR")
+  library(junkR)
   devtools::install_github("rmcelreath/rethinking")
   conflict_prefer('count', 'dplyr')
   conflict_prefer('mutate', 'dplyr')
